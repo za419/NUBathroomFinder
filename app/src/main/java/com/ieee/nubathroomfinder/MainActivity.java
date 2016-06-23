f1e5aa944337fa51dc92b098854001bd254cf9d6
Ryan Hodin <ryan951357@gmail.com>
2016-06-22 21:10:39 -0700

    Uncomment ButterKnife bindings for new imageviews

--- app/src/main/java/com/ieee/nubathroomfinder/MainActivity.java
+++ app/src/main/java/com/ieee/nubathroomfinder/MainActivity.java
@@ -1,25 +1,22 @@
 package com.ieee.nubathroomfinder;
 
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class MainActivity extends AppCompatActivity {
 
     // Bind the maps
     @Bind(R.id.first_floor_image) ImageView firstFloorImage;
     @Bind(R.id.ground_floor_image) ImageView groundFloorImage;
- //   @Bind(R.id.second_floor_image) ImageView secondFloorImage;
- //   @Bind(R.id.third_floor_image) ImageView thirdFloorImage;
- //   @Bind(R.id.fourth_floor_image) ImageView fourthFloorImage;
+    @Bind(R.id.second_floor_image) ImageView secondFloorImage;
+    @Bind(R.id.third_floor_image) ImageView thirdFloorImage;
+    @Bind(R.id.fourth_floor_image) ImageView fourthFloorImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
